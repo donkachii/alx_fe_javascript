a104800291144ac638496b984333dcd08ac41017
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2024-08-05 06:55:36 +0100

    Added JS

---
+++ dom-manipulation/test.js
@@ -0,0 +1,27 @@
+const quotes = [
+  {
+    category: "Programming",
+    quote: [
+      { text: "Talk is cheap. Show me the code.", author: "Kenny Maxwell" },
+      {
+        text: "A bug is a sign that you are making progress.",
+        author: "John Black",
+      },
+      {
+        text: "We code for the world's best solution.",
+        author: "Mitchell Green",
+      },
+    ],
+  },
+  {
+    category: "Psychology",
+    quote: [
+      { text: "Everything starts from the mind.", author: "Rice Campbell" },
+      {
+        text: "The one universal experience is that we’re alive, but not forever.",
+        author: "Henry Weapon",
+      },
+      { text: "Turn your wounds into wisdom.", author: "Amanda Snow" },
+    ],
+  },
+];
